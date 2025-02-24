f4b970474bcca7d082d1acf97c90444e3a4f0cff
KevinComba <kelchospense88@gmail.com>
2025-02-24 18:04:52 +0300

    Implement bidirectional one-to-one relationship between Author and Profile entities; define primary keys and associations in TypeORM

--- 11.Repository-Pattern-TypeORM/src/authors/entities/author.entity.ts
+++ 11.Repository-Pattern-TypeORM/src/authors/entities/author.entity.ts
@@ -1,1 +1,12 @@
-export class Author {}
+import { Profile } from "src/profiles/entities/profile.entity";
+import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Author {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @OneToOne(() => Profile)
+    @JoinColumn()
+    profile: Profile;
+}
--- 11.Repository-Pattern-TypeORM/src/profiles/entities/profile.entity.ts
+++ 11.Repository-Pattern-TypeORM/src/profiles/entities/profile.entity.ts
@@ -1,1 +1,11 @@
-export class Profile {}
+import { Author } from "src/authors/entities/author.entity";
+import { Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Profile {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @OneToOne(() => Author)
+    author: Author;
+}
