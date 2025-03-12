cd72fe0657a3d94bc5149a50401fa146c0d3fe84
KevinComba <kelchospense88@gmail.com>
2025-03-12 13:24:38 +0300

    Add Swagger integration: configure SwaggerModule and set up API documentation

--- 15. Swagger/src/main.ts
+++ 15. Swagger/src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Book Vault API')
+    .setDescription('The Book Vault API description')
+    .setVersion('1.0')
+    .addTag('Books').build();
+
+    const documentFactory = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT') ?? 3000);
