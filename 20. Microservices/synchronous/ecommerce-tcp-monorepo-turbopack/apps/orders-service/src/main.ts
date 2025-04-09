f9c5fc4aaa5bca9f597e9dbd63506e897e2b9eef
KevinComba <kelchospense88@gmail.com>
2025-04-10 02:42:10 +0300

    feat: refactor services to use TCP microservices transport

--- 20. Microservices/synchronous/ecommerce-tcp-monorepo-turbopack/apps/orders-service/src/main.ts
+++ 20. Microservices/synchronous/ecommerce-tcp-monorepo-turbopack/apps/orders-service/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: process.env.ORDERS_SERVICE_HOST || 'localhost',
+      port: Number(process.env.ORDERS_SERVICE_PORT) || 4001,
+    },
+  });
+ 
+  await app.listen();
 }
 bootstrap();
--- 20. Microservices/synchronous/ecommerce-tcp-monorepo-turbopack/apps/users-service/src/main.ts
+++ 20. Microservices/synchronous/ecommerce-tcp-monorepo-turbopack/apps/users-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: process.env.USERS_SERVICE_HOST || 'localhost',
+      port: Number(process.env.USERS_SERVICE_PORT) || 4003,
+    },
+  });
+  await app.listen();
 }
 bootstrap();
